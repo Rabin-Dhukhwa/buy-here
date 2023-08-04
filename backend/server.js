a65efdd51302a31d126c6a011878138cd79b380a
Rabin Dhukhwa <dhukhwarabin@gmail.com>
2023-08-05 06:56:25 +1000

    backend server started, and setup package.json which concurrently run backend and fronend

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+import express from "express";
+import products from "./data/products.js";
+//call dotenv.config() before using any environment variable
+import dotenv from "dotenv";
+dotenv.config();
+
+// to specify the port on which the server will run.
+const port = process.env.PORT || 5000;
+
+//to define our API routes and configure the server.
+const app = express();
+
+app.get("/api/products", (req, res) => {
+  //   res.send("API is running.");
+  res.json(products);
+});
+
+app.get("/api/products/:id", (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(port, () => console.log(`server running on port ${port}`));
