d5b1f08293268bdede4667cf7689dfb5ace0b44a
Rabin Dhukhwa <dhukhwarabin@gmail.com>
2023-12-10 09:24:14 +1100

    Create Review

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -7,6 +7,7 @@ import {
   createProduct,
   updateProduct,
   deleteProduct,
+  createProductReview,
 } from "../controllers/productController.js";
 
 import { protect, admin } from "../middleware/authMiddleware.js";
@@ -26,5 +27,5 @@ router
   .get(getProductById)
   .put(protect, admin, updateProduct)
   .delete(protect, admin, deleteProduct);
-
+router.route("/:id/reviews").post(protect, createProductReview);
 export default router;
